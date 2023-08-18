4a42da1229decde6c79983baa11978321af5c20b
Syed Fazil <syedfazil539@gmail.com>
2023-08-18 07:24:52 +0530

    Implementing dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ import { redirect } from "next/navigation";
 
 export default async function Page() {
   const user = await currentUser();
-  if (!user) return null;
-  const userInfo = await fetchUser(user.id);
-  if (!userInfo.onboarded) redirect("/onboarding");
+
   return (
     <>
       <div className="flex justify-center items-center -mt-2 mb-2">
